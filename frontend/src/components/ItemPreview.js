9dff017d90ba263e80d6cc8b232ed6b5e12885b6
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-11-19 10:17:22 +0530

    Added Placeholder images in case users do not specify one

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import placeholder from '../imgs/placeholder.png';
+
+const placeholder = '/placeholder.png'
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
