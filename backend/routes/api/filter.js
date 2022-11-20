856cdff72a0fa0bd8b2f64be9d51366f5243939a
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-11-20 19:02:58 +0530

    Added query filter by title

--- backend/routes/api/filter.js
+++ backend/routes/api/filter.js
@@ -5,7 +5,7 @@ var Item = mongoose.model("Item");
 // return a list of titles
 router.get("/", function (req, res) {
   const title = req.params.title;
-  const result = Item.filter((item) => item.title === title);
+  const result = Item.filter((item) => item.title == title);
 
   res.status(200).json({
     result,
