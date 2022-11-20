b4f17efdc0a57e0c333bba3afd6e7ea596289ac3
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-11-20 18:08:08 +0530

    Added query filter by title

--- backend/routes/api/filter.js
+++ backend/routes/api/filter.js
@@ -2,12 +2,12 @@ var router = require("express").Router();
 var mongoose = require("mongoose");
 var Item = mongoose.model("Item");
 
-// return a list of tags
+// return a list of titles
 router.get("/", function (req, res, next) {
   Item.find()
-    .distinct("tagList")
-    .then(function (tags) {
-      return res.json({ tags: tags });
+    .distinct("title")
+    .then(function (titles) {
+      return res.json({ titles: titles });
     })
     .catch(next);
 });
